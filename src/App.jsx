f9bb5fed019e02f44166dfc324e00aacb41a5ed9
Suman Saurabh Das <dsumansaurabh@gmail.com>
2024-05-05 12:35:12 +0530

    Created : Re-usable Button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-function App() {
-  return (
-    <h1 className="text-3xl text-sky-600 font-bold">
-      Brainwave 🌊
-    </h1>
-  )
-}
+import ButtonGradient from './assets/svg/ButtonGradient';
+import Button from './components/Button';
 
-export default App
+const App = () => {
+  return (
+    <>
+      <h1 className="text-3xl text-sky-600 font-bold">
+        Brainwave 🌊
+      </h1>
+      <div className="overflow-hidden pt-[4.75rem] lg:pt-[5.25rem]">
+        <Button className="mt-10">
+          Login
+        </Button>
+      </div>
+      {/* ButtonGradient is an svg using which we are giving a gradient effect to the button */}
+      <ButtonGradient />
+    </>
+  )
+};
+
+export default App;
---
+++ src/components/Button.jsx
@@ -0,0 +1,33 @@
+import ButtonSvg from '../assets/svg/ButtonSvg';
+
+// Button is a reusable component.
+// It can export a button or a link based on the href property.
+const Button = ({ className, href, onClick, children, px, white }) => {
+  const classes = `button h-11 inline-flex items-center justify-center relative hover:text-color-1 transition-colors ${px || "px-7"} ${white ? "text-n-8" : "text-n-1"} ${className || ""}`;
+  const spanClasses = "relative z-10";
+
+  // Function to render a button
+  const renderButton = () => {
+    return (
+      <button className={classes}>
+        <span className={spanClasses}>{children}</span>
+        {ButtonSvg(white)}
+      </button>
+    )
+  };
+
+  // Function to render a link
+  const renderLink = () => {
+    return (
+      <a className={classes} href={href}>
+        <span className={spanClasses}>{children}</span>
+        {ButtonSvg(white)}
+      </a>
+    )
+  };
+
+  // Return link if href property exists else return button
+  return href ? renderLink() : renderButton();
+}
+
+export default Button;
